8f19d24db13eabc37f2c9aabf9a52c03725d6ee6
Nayeem <esthieuknayeem@gmail.com>
2024-05-08 05:52:23 +0600

    sdfff

--- brainiaclab/authentication/admin.py
+++ brainiaclab/authentication/admin.py
@@ -1,33 +1,19 @@
 from django.contrib import admin
-
-# Register your models here.
-
-from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
 
-
-from rest_framework.authtoken.admin import TokenAdmin
 
 # TokenAdmin.search_fields = ['user__username']
 
 User = get_user_model()
-# search_fields = ('email',)
-# class CustomUserAdmin(UserAdmin):
-    # list_display = ('phone','email', 'is_varified', 'is_staff', 'is_superuser','is_active')
-    # list_filter = ('is_varified', 'is_staff', 'is_superuser')
-    # fieldsets = (
-    #     (None, {'fields': ('email', 'password')}),
-    #     ('Permissions', {'fields': ('is_varified', 'is_staff', 'is_superuser','is_active','groups')}),
-    # )
-    # add_fieldsets = (
-    #     (None, {
-    #         'classes': ('wide',),
-    #         'fields': ('email','phone', 'password1', 'password2', 'is_varified','is_active', 'is_staff', 'is_superuser','groups'),
-    #     }),
-    # )
+search_fields = ('email',)
 
-    # ordering = ('email',)
-    # filter_horizontal = ()
+from rest_framework.authtoken.admin import TokenAdmin
+from rest_framework.authtoken.models import Token
+
+class CustomTokenAdmin(TokenAdmin):
+    search_fields = ('key', 'user__username')  # Adjust fields as per your model
+
+admin.site.register(Token, CustomTokenAdmin)
 
 
 
