650fbb5c33d97a9ee8165284608a1f24f18d2774
Nayeem <esthieuknayeem@gmail.com>
2024-05-08 05:55:44 +0600

    lllll

--- brainiaclab/authentication/admin.py
+++ brainiaclab/authentication/admin.py
@@ -7,13 +7,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 search_fields = ('email',)
 
-from rest_framework.authtoken.admin import TokenAdmin
-from rest_framework.authtoken.models import Token
 
-class CustomTokenAdmin(TokenAdmin):
-    search_fields = ('key', 'user__username')  # Adjust fields as per your model
-
-admin.site.register(Token, CustomTokenAdmin)
 
 
 
